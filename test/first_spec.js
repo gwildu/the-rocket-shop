e894219c7cc0ca1394da38b2df7eaf55adac3bd3
mrickenbacher <marius.rickenbacher@ricardo.ch>
2017-03-29 15:30:54 +0200

    fixed the test

--- test/first_spec.js
+++ test/first_spec.js
@@ -5,7 +5,7 @@ let assert = require('assert')
 describe('A feature', function() {
     describe('A scenarioooo', function() {
         it('does something expected', function() {
-            assert(false, 'oh oh')
+            assert(true)
         })
     })
 })
